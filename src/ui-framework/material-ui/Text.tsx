07c53d762fea54427ae6d2878f859a91d4a4a8df
VISWANATHAN. S <142203206+viswavich@users.noreply.github.com>
2024-08-13 23:29:38 +0530

    Add files via upload

---
+++ src/ui-framework/material-ui/Text.tsx
@@ -0,0 +1,9 @@
+
+
+export const Text = ({ value }: { value: string }) => {
+    return (
+        <>
+            <input type='text' style={{width:'100px',height:'30px',margin:10,padding:5,borderRadius:'5px'}} value={value}></input>
+        </>
+    )
+}
