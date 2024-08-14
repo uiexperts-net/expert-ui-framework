2d6f287e92cd2b9cd697ac9b55f960b93982acee
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2024-08-14 18:19:37 +0530

    Revert "UI framework init"

--- src/ui-framework/material-ui/Text.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const Text = ({ value }: { value: string }) => {
-    return (
-        <>
-            <input type='text' style={{width:'100px',height:'30px',margin:10,padding:5,borderRadius:'5px'}} value={value}></input>
-        </>
-    )
-}
