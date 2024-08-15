9dc9170e87441ca0726a97b180e9c50a1716b584
shyamsgru <ashyamraj741@gmail.com>
2024-08-15 12:27:38 +0530

    set_env
    15-08-2024

---
+++ vite-project - 1/vite-project/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+};
+
+export default config;
