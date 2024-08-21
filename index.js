82cb66f225e5695697396df2b3ab0e683c350d05
Jameeru <144582911+Jameeru@users.noreply.github.com>
2024-08-21 13:54:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import { PaperProvider } from 'react-native-paper';
+import App from './storybook'
+export default function Main() {
+    return (
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    );
+  }
+AppRegistry.registerComponent(appName, () => App);
