e7b607534c4761e348f8b0c7d89ae988b5232002
Jameeru <144582911+Jameeru@users.noreply.github.com>
2024-08-22 21:19:14 +0530

    Add files via upload
    Button

---
+++ index.js
@@ -0,0 +1,11 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+if (__DEV__) {
+  import('./storybook').then(({ default: StorybookUI }) => {
+    AppRegistry.registerComponent(appName, () => StorybookUI);
+  });
+} else {
+  AppRegistry.registerComponent(appName, () => App);
+}
