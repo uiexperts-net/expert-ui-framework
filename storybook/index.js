cf302e96120df31cf8ee97833526286ff1818f1a
Jameeru <144582911+Jameeru@users.noreply.github.com>
2024-09-02 18:00:54 +0530

    Update index.js

--- storybook/index.js
+++ storybook/index.js
@@ -11,6 +11,39 @@ const req = require.context('../src/stories', true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+// Import stories
+const req = require.context('../src/stories', true, /\.stories\.tsx$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+// Configure Storybook
+configure(loadStories, module);
+
+const StorybookUIRoot = getStorybookUI({});
+
+// Register Storybook as a React Native app
+AppRegistry.registerComponent('SwitchSliderApp', () => StorybookUIRoot);
+configure(() => {
+  require('../src/stories/FloatingActionButton.stories');
+}, module);
+// Import stories
+configure(() => {
+  require('../src/stories/ToggleButton.stories');
+}, module);
+
+const StorybookUIRoot = getStorybookUI({});
+AppRegistry.registerComponent('TransferListApp', () => StorybookUIRoot);
+
+const StorybookUIRoot = getStorybookUI({});
+AppRegistry.registerComponent('TransferListApp', () => StorybookUIRoot);
+configure(() => {
+  require('../src/stories/RatingButton.stories.tsx');
+}, module);
+
+const StorybookUIRoot = getStorybookUI({});
+
+AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 configure(() => {
   require('../src/stories/TransferList.stories');
 }, module);
