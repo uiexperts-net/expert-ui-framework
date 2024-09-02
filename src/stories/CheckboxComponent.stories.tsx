e33ae0259c12a08d30efe5a59938793f37bebb5f
Jameeru <144582911+Jameeru@users.noreply.github.com>
2024-09-02 18:12:58 +0530

    Update CheckboxComponent.stories.tsx

--- src/stories/CheckboxComponent.stories.tsx
+++ src/stories/CheckboxComponent.stories.tsx
@@ -1,9 +1,23 @@
-import { addons } from '@storybook/addons';
-import { create } from '@storybook/theming';
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import CheckboxComponent from '.src/components/CheckboxComponent';
+import { action } from '@storybook/addon-actions';
 
-addons.setConfig({
-  theme: create({
-    base: 'light',
-    brandTitle: 'MyApp Storybook',
-  }),
+storiesOf('CheckboxComponent', module)
+  .add('Default', () => <CheckboxExample />)
+  .add('Checked Option 1', () => {
+    const [checked1, setChecked1] = useState<boolean>(true);
+    const [checked2, setChecked2] = useState<boolean>(false);
+    const [checked3, setChecked3] = useState<boolean>(false);
+
+    return (
+      <View style={styles.container}>
+        <CheckboxExample />
+      </View>
+    );
+  })
+  .add('All Checked', () => {
+    const [checked1, setChecked1] = useState<boolean>(true);
+    const [checked2, setChecked2] = useState<boolean>(true);
+    const [checked3, setChecked3] = useState<boolean>(true);
 });
