73cfb359f2fb3f5f1250982a0854fba0c0890b13
Abdul kalam k <100997406+Abdulkalam24@users.noreply.github.com>
2024-09-03 17:47:42 +0530

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,7 @@
-
 import React from 'react';
-=======
-
-import ReactDOM from 'react-dom';
-
-
-
-
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-
-
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import App from './App';
+import App from './App.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -35,24 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for React 18+
-import './index.css'; // Ensure this file exists
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
