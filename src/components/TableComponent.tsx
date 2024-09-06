0660fce6e11d0414f569be9b3482977d05361218
Abdul kalam k <100997406+Abdulkalam24@users.noreply.github.com>
2024-09-06 12:04:10 +0530

    Add files via upload

---
+++ src/components/TableComponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TableComponent = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John Doe</td>
+          <td>30</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default TableComponent;
