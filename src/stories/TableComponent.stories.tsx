a450bb3fb6b334a977a0f6aba182771735b63ad6
Abdul kalam k <100997406+Abdulkalam24@users.noreply.github.com>
2024-09-06 12:07:25 +0530

    Add files via upload

---
+++ src/stories/TableComponent.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TableComponent from '../components/TableComponent';
+
+export default {
+  title: 'Components/Table',
+  component: TableComponent,
+};
+
+export const Default = () => <TableComponent />;
